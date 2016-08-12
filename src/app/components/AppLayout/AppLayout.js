c156c42da51ab9308d13d0d0b2ce5a59933e0ea1
Kevin Wang <chunkai1312@gmail.com>
2016-08-12 12:18:07 +0800

    feat: add AppLayout component

---
+++ src/app/components/AppLayout/AppLayout.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import { Layout } from 'react-toolbox/lib/layout';
+import theme from './theme.scss';
+
+const AppLayout = (props) => <Layout {...props} theme={theme} />;
+
+export default AppLayout;
