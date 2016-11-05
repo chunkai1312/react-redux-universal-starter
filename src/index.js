7446b2c7303a46a7bdff183d28b0b9a682c1ee1b
Chun-Kai Wang <chunkai1312@gmail.com>
2016-11-06 01:35:19 +0800

    refactor(*): src structure

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 import 'react-toolbox/lib/commons.scss'
-
 import 'babel-polyfill'
 import React from 'react'
 import { render } from 'react-dom'
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import { Route, IndexRoute } from 'react-router'
-import App from './containers/App'
-import HomePage from './containers/HomePage'
-import AboutPage from './containers/AboutPage'
+import App from '../containers/App'
+import HomePage from '../containers/HomePage'
+import AboutPage from '../containers/AboutPage'
 
 export default (
   <Route path="/" component={App}>
