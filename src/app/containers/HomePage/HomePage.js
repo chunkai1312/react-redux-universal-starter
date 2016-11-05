d6c982c0bea3cbecc31c457f6c41c9424ea0c620
Chun-Kai Wang <chunkai1312@gmail.com>
2016-11-05 23:53:35 +0800

    refactor(containers): add HomePage

---
+++ src/app/containers/HomePage/HomePage.js
@@ -0,0 +1,43 @@
+import React, { Component, PropTypes } from 'react'
+import { bindActionCreators } from 'redux'
+import { connect } from 'react-redux'
+import { Counter } from '../../components'
+import * as layoutActions from '../../actions/layout'
+import * as counterActions from '../../actions/counter'
+
+class HomePage extends Component {
+  static propTypes = {
+    layout: PropTypes.object.isRequired,
+    counter: PropTypes.number.isRequired,
+    actions: PropTypes.object.isRequired
+  }
+
+  componentWillMount () {
+    this.props.actions.setPageTitle('Home')
+  }
+
+  render () {
+    const { counter, actions } = this.props
+    return (
+      <div style={{ padding: '2.4rem' }}>
+        <Counter
+          counter={counter}
+          increment={actions.increment}
+          decrement={actions.decrement}
+          incrementAsync={actions.incrementAsync}
+        />
+      </div>
+    )
+  }
+}
+
+const mapStateToProps = (state) => ({
+  layout: state.layout,
+  counter: state.counter
+})
+
+const mapDispatchToProps = (dispatch) => ({
+  actions: bindActionCreators(Object.assign({}, layoutActions, counterActions), dispatch)
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(HomePage)
