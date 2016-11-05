21017b86be7a57a2c70fd5ac203a7463d6220a9b
Chun-Kai Wang <chunkai1312@gmail.com>
2016-11-06 01:50:41 +0800

    refactor(*): move to routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import { Route, IndexRoute } from 'react-router'
 import App from '../containers/App'
-import HomePage from '../containers/HomePage'
-import AboutPage from '../containers/AboutPage'
+import HomePage from './HomePage'
+import AboutPage from './AboutPage'
 
 export default (
   <Route path="/" component={App}>
