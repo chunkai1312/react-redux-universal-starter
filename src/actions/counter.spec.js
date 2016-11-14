9b3b9907cfa5186ad619e90cd0416accc0047a38
Chun-Kai Wang <chunkai1312@gmail.com>
2016-11-15 02:07:27 +0800

    test(actions): add counter action test

---
+++ src/actions/counter.spec.js
@@ -0,0 +1,35 @@
+import { expect } from 'chai'
+import * as actions from './counter'
+import * as types from '../constants/actionTypes'
+
+describe('Counter Actions:', () => {
+  describe('#increment()', () => {
+    it('should be a function', () => {
+      expect(actions.increment).to.be.a('function')
+    })
+
+    it('should create an action with type "COUNTER_INCREMENT"', () => {
+      expect(actions.increment()).to.eql({ type: types.COUNTER_INCREMENT })
+    })
+  })
+
+  describe('#decrement()', () => {
+    it('should be a function', () => {
+      expect(actions.decrement).to.be.a('function')
+    })
+
+    it('should create an action with type "COUNTER_DECREMENT"', () => {
+      expect(actions.decrement()).to.eql({ type: types.COUNTER_DECREMENT })
+    })
+  })
+
+  describe('#incrementAsync()', () => {
+    it('should be a function', () => {
+      expect(actions.incrementAsync).to.be.a('function')
+    })
+
+    it('should create an action with type "COUNTER_INCREMENT_ASYNC"', () => {
+      expect(actions.incrementAsync()).to.eql({ type: types.COUNTER_INCREMENT_ASYNC })
+    })
+  })
+})
