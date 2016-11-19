0569a276d267e73ebf2884069989ba0872905570
Chun-Kai Wang <chunkai1312@gmail.com>
2016-11-20 01:25:04 +0800

    refactor(sagas): update

--- src/sagas/appLayout.js
+++ src/sagas/appLayout.js
@@ -3,10 +3,6 @@ import { fork, put } from 'redux-saga/effects'
 import { push, goBack } from 'react-router-redux'
 import * as types from '../constants/actionTypes'
 
-export function * backToHome (action) {
-  yield put(push('/'))
-}
-
 export function * clickMenuItem (action) {
   const menuItem = action.payload
   yield put(push(menuItem.path))
@@ -16,8 +12,8 @@ export function * clickBackButton (action) {
   yield put(goBack())
 }
 
-function * watchBackToHome () {
-  yield * takeEvery(types.BACK_TO_HOME, backToHome)
+export function * backToHome (action) {
+  yield put(push('/'))
 }
 
 function * watchClickMenuItem () {
@@ -28,10 +24,14 @@ function * watchClickBackButton () {
   yield * takeEvery(types.CLICK_BACK_BUTTON, clickBackButton)
 }
 
+function * watchBackToHome () {
+  yield * takeEvery(types.BACK_TO_HOME, backToHome)
+}
+
 export default function * () {
   yield [
-    fork(watchBackToHome),
     fork(watchClickMenuItem),
-    fork(watchClickBackButton)
+    fork(watchClickBackButton),
+    fork(watchBackToHome)
   ]
 }
--- src/sagas/routing.js
+++ src/sagas/routing.js
@@ -3,7 +3,7 @@ import { fork, put } from 'redux-saga/effects'
 import { LOCATION_CHANGE } from 'react-router-redux'
 import * as actions from '../actions'
 
-export function * locationChange (action) {
+export function * setPageTitle (action) {
   const path = {
     '/': () => actions.setPageTitle('Home'),
     '/about': () => actions.setPageTitle('About')
@@ -12,7 +12,7 @@ export function * locationChange (action) {
 }
 
 function * watchLocationChange (state) {
-  yield * takeEvery(LOCATION_CHANGE, locationChange)
+  yield * takeEvery(LOCATION_CHANGE, setPageTitle)
 }
 
 export default function * () {
