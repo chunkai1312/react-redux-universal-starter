291ffac0a823256d90334d9ad7586f1ac732d423
Chun-Kai Wang <chunkai1312@gmail.com>
2016-11-20 00:22:15 +0800

    refactor(*): use redux devtools extension

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,5 +1,33 @@
-if (process.env.NODE_ENV === 'production') {
-  module.exports = require('./configureStore.prod')
-} else {
-  module.exports = require('./configureStore.dev')
+import { createStore, applyMiddleware, compose } from 'redux'
+import { browserHistory } from 'react-router'
+import { routerMiddleware } from 'react-router-redux'
+import createSagaMiddleware from 'redux-saga'
+import rootReducer from '../reducers'
+import rootSaga from '../sagas'
+
+const configureStore = preloadedState => {
+  const routeringMiddleware = routerMiddleware(browserHistory)
+  const sagaMiddleware = createSagaMiddleware()
+  const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+
+  const store = createStore(
+    rootReducer,
+    preloadedState,
+    composeEnhancers(
+      applyMiddleware(routeringMiddleware, sagaMiddleware)
+    )
+  )
+
+  sagaMiddleware.run(rootSaga)
+
+  if (module.hot) {
+    module.hot.accept('../reducers', () => {
+      const nextRootReducer = require('../reducers').default
+      store.replaceReducer(nextRootReducer)
+    })
+  }
+
+  return store
 }
+
+export default configureStore
