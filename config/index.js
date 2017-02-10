bc744d66395c981d72a991e527496096c1d101a6
Chun-Kai Wang <chunkai1312@gmail.com>
2017-02-10 16:34:13 +0800

    refactor(config): remove

--- config/index.js
+++
@@ -1,8 +0,0 @@
-'use strict'
-
-const _ = require('lodash')
-const localConfig = require('./local')
-const envConfig = require(`./env/${localConfig.env}`)
-const config = _.merge(localConfig, envConfig)
-
-module.exports = config
