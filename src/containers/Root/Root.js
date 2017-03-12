b0ff557b230e25ce9273c4182e35be3aeb27fd3f
Chun-Kai Wang <chunkai1312@gmail.com>
2017-03-12 11:54:59 +0800

    refactor(containers): Root

--- src/containers/Root/Root.js
+++ src/containers/Root/Root.js
@@ -1,16 +1,19 @@
 import React, { Component, PropTypes } from 'react'
 import { Provider } from 'react-redux'
-import { Router } from 'react-router'
+import { Router, RouterContext } from 'react-router'
 import { AppContainer } from 'react-hot-loader'
 import getRoutes from '../../routes'
 
 class Root extends Component {
   render () {
-    const { store, history } = this.props
+    const { store, history, renderProps } = this.props
     return (
       <AppContainer>
         <Provider store={store}>
-          <Router key={Math.random()} history={history} routes={getRoutes()} />
+          { __SERVER__
+            ? <RouterContext {...renderProps} />
+            : <Router key={Math.random()} history={history} routes={getRoutes()} />
+          }
         </Provider>
       </AppContainer>
     )
@@ -19,7 +22,8 @@ class Root extends Component {
 
 Root.propTypes = {
   store: PropTypes.object.isRequired,
-  history: PropTypes.object.isRequired
+  history: PropTypes.object.isRequired,
+  renderProps: PropTypes.object
 }
 
 export default Root
