85fe113ed2862bda968a8a46779c2fa152b5f559
Chun-Kai Wang <chunkai1312@gmail.com>
2017-03-12 10:57:07 +0800

    refactor: routes

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,10 @@
 import React from 'react'
-import { Route, IndexRoute, Redirect } from 'react-router'
+import { Route, IndexRoute } from 'react-router'
 import { App, Home, About } from './containers'
 
 export default () => (
   <Route path="/" component={App}>
     <IndexRoute component={Home} />
     <Route path="/about" component={About} />
-    <Redirect from="/test" to="/about" />
   </Route>
 )
