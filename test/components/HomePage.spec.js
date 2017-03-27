a093379a922d909eb131d00d185d6e85d050b799
Chun-Kai Wang <chunkai1312@gmail.com>
2017-03-27 23:59:00 +0800

    test(components): HomePage

---
+++ test/components/HomePage.spec.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+import toJson from 'enzyme-to-json'
+import Helmet from 'react-helmet'
+import HomePage from '../../src/components/HomePage'
+
+describe('<HomePage />', () => {
+  function setup () {
+    const wrapper = shallow(<HomePage />)
+    return { wrapper }
+  }
+
+  it('should render a <Helmet /> component', () => {
+    const { wrapper } = setup()
+    expect(wrapper.find(Helmet)).toHaveLength(1)
+    expect(wrapper.find(Helmet).node.props.title).toBe('Home')
+  })
+
+  it('should render correctly', () => {
+    const { wrapper } = setup()
+    expect(toJson(wrapper)).toMatchSnapshot()
+  })
+})
